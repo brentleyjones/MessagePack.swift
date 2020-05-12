5c52bb2aa23d8e7f48260f26fb46092bbdc334ad
Brentley Jones <github@brentleyjones.com>
2020-05-11 21:03:10 -0500

    Update Package.swift to 5.1 format

--- Package.swift
+++ Package.swift
@@ -1,3 +1,23 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
-let package = Package(name: "MessagePack")
+let package = Package(
+    name: "MessagePack",
+    products: [
+        .library(
+            name: "MessagePack",
+            targets: ["MessagePack"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "MessagePack",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "MessagePackTests",
+            dependencies: ["MessagePack"]
+        ),
+    ]
+)
